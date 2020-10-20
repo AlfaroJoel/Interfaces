a196259c77a08a55185d50bfd3b39dbef0eaebde
AlfaroJoel <cholealfaro77@gmail.com>
2020-10-20 18:11:05 -0300

    Agrego hover 3d, Trabajo terminado

--- Entregable3/js/comentario.js
+++ Entregable3/js/comentario.js
@@ -2,36 +2,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let form = document.querySelector(".form").childNodes;
     let btnComment = document.querySelector(".comentar");
-    let check = false;
+    let checkCom = false;
+    let checkPara = false;
+    let checkNombre = false;
 
     btnComment.addEventListener("click", function(){
         if(form[3].value[0] == " " || form[3].value[0] == undefined || form[3].value[0] == ""){
             form[3].style.animation = "moveinput 0.7s linear";
             form[3].style.border = "2px solid red";
-            check = false;
+            form[15].innerHTML = "";
+            checkPara = false;
         }else{
             form[3].style.animation = "";
+            form[15].innerHTML = "";
             form[3].style.border = "2px solid green";
-            check = true;
+            checkPara = true;
         }
         if(form[7].value[0] == " " || form[7].value[0] == undefined || form[7].value[0] == ""){
             form[7].style.border = "2px solid red";
+            form[15].innerHTML = "";
             form[7].style.animation = "moveinput 0.7s linear 0.8s";
-            check = false;
+            checkNombre = false;
         }else{
             form[7].style.animation = "";
+            form[15].innerHTML = "";
             form[7].style.border = "2px solid green";
-            check = true;
+            checkNombre = true;
         }
         if(form[11].value[0] == " " || form[11].value[0] == undefined || form[11].value[0] == ""){
             form[11].style.border = "2px solid red";
-            check = false;
+            form[15].innerHTML = "";
+            checkCom = false;
         }else{
             form[11].style.border = "2px solid green";
-            check = true;
+            form[15].innerHTML = "";
+            checkCom = true;
         }
 
-        if(check){
+        if(checkCom && checkPara && checkNombre){
             btnComment.style.animation = "";
             form[15].innerHTML = "Comentario enviado con exito";
             form[15].style.color = "green";
